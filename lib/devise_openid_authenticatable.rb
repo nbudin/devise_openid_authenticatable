0e5717582582ae741131d545c2cfd0cfb3a238dc
Nat Budin <natbudin@gmail.com>
2010-05-21 14:12:09 -0400

    Fix routes to work with both Rails 2 and 3

--- lib/devise_openid_authenticatable.rb
+++ lib/devise_openid_authenticatable.rb
@@ -2,8 +2,10 @@ require 'devise'
 
 require 'devise_openid_authenticatable/schema'
 require 'devise_openid_authenticatable/strategy'
+require 'devise_openid_authenticatable/routes'
 
 Devise.add_module(:openid_authenticatable, 
   :strategy => true,
   :model => 'devise_openid_authenticatable/model',
+  :controller => :sessions,
   :route => :openid)
--- lib/devise_openid_authenticatable/routes.rb
+++ lib/devise_openid_authenticatable/routes.rb
@@ -1,11 +1,16 @@
-require 'action_dispatch/routing/mapper'
-
-module ActionDispatch::Routing
-  class Mapper
+if ActionController::Routing.name =~ /ActionDispatch/
+  # We're on Rails 3
+  ActionDispatch::Routing::Mapper.class_eval do
     protected
   
-    def devise_openid(mapping, controllers)
-      devise_session(mapping, controllers)
-    end
+    alias_method :devise_openid, :devise_session
+  end
+else
+  # We're on Rails 2
+
+  ActionController::Routing::RouteSet::Mapper.class_eval do
+    protected
+
+    alias_method :openid_authenticatable, :database_authenticatable
   end
 end
