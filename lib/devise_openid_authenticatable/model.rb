d84d1d9d3c8030adca30a26c7db63880461ed33c
Nat Budin <natbudin@gmail.com>
2010-05-21 14:12:22 -0400

    Add find_by_identity_url method

--- lib/devise_openid_authenticatable/model.rb
+++ lib/devise_openid_authenticatable/model.rb
@@ -1,6 +1,9 @@
 module Devise
   module Models
     module OpenidAuthenticatable
+      def find_by_identity_url(identity_url)
+        find(:first, :conditions => {:identity_url => identity_url})
+      end
     end
   end
 end
