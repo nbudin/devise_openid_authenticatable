0546e9bdf438dee653b800bc4cf5d94006cd914e
Neil L. Decapia <neildecapia@gmail.com>
2010-05-25 06:38:07 +0800

    Making #find_by_identity_url a class method

--- lib/devise_openid_authenticatable/model.rb
+++ lib/devise_openid_authenticatable/model.rb
@@ -1,8 +1,14 @@
 module Devise
   module Models
     module OpenidAuthenticatable
-      def find_by_identity_url(identity_url)
-        find(:first, :conditions => {:identity_url => identity_url})
+      def self.included(base)
+        base.extend ClassMethods
+      end
+
+      module ClassMethods
+        def find_by_identity_url(identity_url)
+          find(:first, :conditions => {:identity_url => identity_url})
+        end
       end
     end
   end
