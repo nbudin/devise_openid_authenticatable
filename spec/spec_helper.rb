ea2c79ac12b9b56377cbea35d5d35dfd4509682b
Nat Budin <natbudin@gmail.com>
2010-09-04 11:48:14 -0400

    Use ROTS and ShamRack for more real-worldish testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,40 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require "scenario/config/environment"
 require "rails/test_help"
 require 'rspec/rails'
+require 'sham_rack'
+require 'rots'
 
 Webrat.configure do |config|
   config.mode = :rails
   config.open_error_files = false
 end
+
+# This is mostly copied in from bin/rots; they don't provide a single app class we could just reuse, unfortunately
+
+rots_config = YAML.load(<<-ROTS_CONFIG)
+# Default configuration file
+identity: john.doe
+sreg:
+  nickname: jdoe
+  fullname: John Doe
+  email: jhon@doe.com
+  dob: 1985-09-21
+  gender: M
+ROTS_CONFIG
+
+rots_server_options = {
+  :storage => File.join(File.dirname(__FILE__), 'scenario', 'tmp', 'rots')
+}
+rots_server = Rack::Builder.new do 
+  use Rack::Lint
+  map ("/%s" % rots_config['identity']) do
+    run Rots::IdentityPageApp.new(rots_config, rots_server_options)
+  end
+  map "/server" do
+    run Rots::ServerApp.new(rots_config, rots_server_options)
+  end
+end
+
+ShamRack.mount(rots_server, 'openid.example.org')
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
