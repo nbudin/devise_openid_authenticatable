99e88e750c75f791c366c80450327cc7a676e634
Nat Budin <nbudin@patientslikeme.com>
2012-09-25 14:22:50 -0400

    Make the tests work in Devise >= 2.1. A bit hacky, but what can you do...

--- spec/scenario/db/migrate/20100401102949_create_tables.rb
+++ spec/scenario/db/migrate/20100401102949_create_tables.rb
@@ -1,20 +1,31 @@
+require 'devise/version'
+
+def openid_authenticatable_fields(t)
+  if Devise::VERSION < "2.1"
+    require 'devise/schema'
+    t.openid_authenticatable
+  else
+    t.string :identity_url
+  end
+end
+
 class CreateTables < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.openid_authenticatable
+      openid_authenticatable t
       t.rememberable
       t.string :email
       t.timestamps
     end
     
     create_table :database_users do |t|
-      t.openid_authenticatable
+      openid_authenticatable t
       t.database_authenticatable
       t.timestamps
     end
     
     create_table :legacy_users do |t|
-      t.openid_authenticatable
+      openid_authenticatable t
       t.rememberable
       t.string :email
       t.timestamps
