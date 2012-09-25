ad821470370e778fd0038ce509d860c714941232
Nat Budin <nbudin@patientslikeme.com>
2012-09-25 13:22:16 -0400

    Don't hard-depend on devise/schema

--- lib/devise_openid_authenticatable/schema.rb
+++ lib/devise_openid_authenticatable/schema.rb
@@ -1,9 +1,15 @@
-Devise::Schema.class_eval do
-  def openid_authenticatable
-    if respond_to?(:apply_devise_schema)
-      apply_devise_schema :identity_url, String
-    else
-      apply_schema :identity_url, String
+require 'devise/version'
+
+# Devise 2.1 removes schema stuff
+if Devise::VERSION < "2.1"
+  require 'devise/schema'
+  Devise::Schema.class_eval do
+    def openid_authenticatable
+      if respond_to?(:apply_devise_schema)
+        apply_devise_schema :identity_url, String
+      else
+        apply_schema :identity_url, String
+      end
     end
   end
 end
