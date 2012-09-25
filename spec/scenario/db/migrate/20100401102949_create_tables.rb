f72b5221c8752f094fcb03bf7cb041977fac5ba2
Nat Budin <nbudin@patientslikeme.com>
2012-09-25 14:40:33 -0400

    Get rid of the other old migration helpers too

--- spec/scenario/db/migrate/20100401102949_create_tables.rb
+++ spec/scenario/db/migrate/20100401102949_create_tables.rb
@@ -1,7 +1,5 @@
 require 'devise/version'
 
-
-
 class CreateTables < ActiveRecord::Migration
   def self.openid_authenticatable_fields(t)
     if Devise::VERSION < "2.1"
@@ -12,23 +10,38 @@ class CreateTables < ActiveRecord::Migration
     end
   end
 
+  def self.rememberable_fields(t)
+    if Devise::VERSION < "2.1"
+      require 'devise/schema'
+      t.rememberable
+    else
+      t.datetime :remember_created_at
+    end
+  end
+
   def self.up
     create_table :users do |t|
       openid_authenticatable_fields t
-      t.rememberable
+      rememberable_fields t
       t.string :email
       t.timestamps
     end
     
     create_table :database_users do |t|
       openid_authenticatable_fields t
-      t.database_authenticatable
+      if Devise::VERSION < "2.1"
+        t.database_authenticatable
+      else
+        t.string :email,              :null => false, :default => ""
+        t.string :encrypted_password, :null => false, :default => ""
+      end
+
       t.timestamps
     end
     
     create_table :legacy_users do |t|
       openid_authenticatable_fields t
-      t.rememberable
+      rememberable_fields t
       t.string :email
       t.timestamps
     end
