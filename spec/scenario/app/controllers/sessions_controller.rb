a0d1c14a9cb333eb90a8f832a9027a56dcdc7057
Nat Budin <nbudin@patientslikeme.com>
2013-08-20 10:05:33 -0400

    We don't need to override SessionsController and doing so is causing issues in Devise 3.0

--- spec/scenario/app/controllers/sessions_controller.rb
+++
@@ -1,4 +0,0 @@
-class SessionsController < Devise::SessionsController
-  def new
-  end
-end
--- spec/scenario/config/routes.rb
+++ spec/scenario/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :sessions => 'sessions' }
-  devise_for :database_users, :controllers => { :sessions => 'sessions' }
-  devise_for :legacy_users, :controllers => { :sessions => 'sessions' }
+  devise_for :users
+  devise_for :database_users
+  devise_for :legacy_users
   root :to => "home#index"
 end
